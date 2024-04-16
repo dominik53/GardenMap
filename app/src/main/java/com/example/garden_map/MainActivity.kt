ab5e45a8232ca196191118afe38a980eaa14afb0
dominik53 <domikij981@student.polsl.pl>
2024-04-16 13:36:40 +0200

    init2

--- app/src/main/java/com/example/garden_map/MainActivity.kt
+++ app/src/main/java/com/example/garden_map/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
-//test
+//testowanie2
     override fun onMapReady(googleMap: GoogleMap) {
         mGoogleMap = googleMap
     }
