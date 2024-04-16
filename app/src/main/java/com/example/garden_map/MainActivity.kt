8d92f6e3d07e9237781c84603e5839a43eca6c58
NuzzGO <72353559+NuzzGO@users.noreply.github.com>
2024-04-16 13:57:22 +0200

    Dodanie 1111

--- app/src/main/java/com/example/garden_map/MainActivity.kt
+++ app/src/main/java/com/example/garden_map/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
-//testowanie4
+//testowanie1111
     override fun onMapReady(googleMap: GoogleMap) {
         mGoogleMap = googleMap
     }
