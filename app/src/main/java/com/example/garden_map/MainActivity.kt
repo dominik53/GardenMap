3b5334473b30c9925bd55c1454ed4bd820a16f32
dominik53 <domikij981@student.polsl.pl>
2024-05-07 12:04:10 +0200

    123123123

--- app/src/main/java/com/example/garden_map/MainActivity.kt
+++ app/src/main/java/com/example/garden_map/MainActivity.kt
@@ -20,20 +20,14 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.MarkerOptions
 import android.widget.Button
-import androidx.fragment.app.FragmentTransaction
-import android.view.View
-import android.content.Context
-import com.google.android.gms.maps.model.PolylineOptions
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var binding: ActivityMainBinding
-    private lateinit var mapFragment: SupportMapFragment
     private var mGoogleMap:GoogleMap? = null
-    private val markerPositions: MutableList<LatLng> = mutableListOf()
-    private val waypointsToConnect: MutableList<LatLng> = mutableListOf()
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,22 +37,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Initialize mapFragment
-        mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
-
-        // Listen for destination changes
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            // Show or hide the map fragment based on the current destination
-            if (destination.id == R.id.navigation_home) {
-                showMapFragment()
-                showWaypointButton()
-            } else {
-                hideMapFragment()
-                hideWaypointButton()
-            }
-        }
-
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
@@ -68,7 +48,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
 
         /* LOKALIZACJA */
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
@@ -110,16 +89,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
-    override fun onStop() {
-        super.onStop()
-        saveMarkerPositions() // Save markers before closing the app
-    }
-
-    override fun onStart() {
-        super.onStart()
-        loadMarkers() // Load saved markers when the app starts
-    }
-
     override fun onMapReady(googleMap: GoogleMap) {
         mGoogleMap = googleMap
 
@@ -147,8 +116,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             .addOnSuccessListener { location ->
                 // Got last known location. In some rare situations, this can be null.
                 if (location != null) {
-                    val currentLatLng = LatLng(location.latitude, location.longitude)
-                    mGoogleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, DEFAULT_ZOOM))
+                    // Use location.latitude and location.longitude to get the user's location
                 }
             }
     }
@@ -172,142 +140,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     companion object {
         private const val REQUEST_LOCATION_PERMISSION = 1
         private const val DEFAULT_ZOOM = 15f
-    }
-
-    private fun showMapFragment() {
-        // Show the map fragment
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.show(mapFragment)
-        transaction.commit()
-    }
-
-    private fun hideMapFragment() {
-        // Hide the map fragment
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.hide(mapFragment)
-        transaction.commit()
-    }
-
-    private fun showWaypointButton() {
-        val addWaypointButton: Button = findViewById(R.id.addWaypointButton)
-        addWaypointButton.visibility = View.VISIBLE
-    }
-
-    private fun hideWaypointButton() {
-        val addWaypointButton: Button = findViewById(R.id.addWaypointButton)
-        addWaypointButton.visibility = View.GONE
-    }
-
-    // Function to save marker positions to SharedPreferences
-    private fun saveMarkerPositions() {
-        // Ensure proper reference to the Context class
-        val sharedPreferences = this.getSharedPreferences("Markers", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.clear() // Clear existing markers before saving new ones
-        markerPositions.forEachIndexed { index, latLng ->
-            val latLngString = "${latLng.latitude},${latLng.longitude}"
-            editor.putString("marker_$index", latLngString)
-        }
-        editor.putInt("marker_count", markerPositions.size)
-        editor.apply()
-    }
-
-    // Function to load markers from SharedPreferences
-    private fun loadMarkers() {
-        // Ensure proper reference to the Context class
-        val sharedPreferences = this.getSharedPreferences("Markers", Context.MODE_PRIVATE)
-        val markerCount = sharedPreferences.getInt("marker_count", 0)
-        val googleMap = mGoogleMap ?: return
-        for (i in 0 until markerCount) {
-            val latLngString = sharedPreferences.getString("marker_$i", null) ?: continue
-            val (lat, lng) = latLngString.split(",").map { it.toDouble() }
-            val markerLatLng = LatLng(lat, lng)
-            googleMap.addMarker(
-                MarkerOptions()
-                    .position(markerLatLng)
-                    .title("Waypoint")
-            )
-        }
-    }
-
-    private fun addMarkerPosition(markerLatLng: LatLng) {
-        markerPositions.add(markerLatLng)
-    }
-
-    //markery
-    fun onAddMarkerButtonClick(view: View) {
-        // Dodaj nowy marker na mapie
-        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-            location?.let {
-                val currentLatLng = LatLng(location.latitude, location.longitude)
-                mGoogleMap?.addMarker(
-                    MarkerOptions()
-                        .position(currentLatLng)
-                        .title("Waypoint")
-                )
-                // Dodaj punkt do listy punktów
-                addMarkerPosition(currentLatLng)
-                // Połącz markery liniami
-                connectMarkers()
-            }
-        }
-    }
-
-    private fun connectMarkers() {
-        val googleMap = mGoogleMap ?: return
-        if (markerPositions.size > 1) {
-            val lastLatLng = markerPositions[markerPositions.size - 2]
-            val currentLatLng = markerPositions.last()
-            googleMap.addPolyline(PolylineOptions().add(lastLatLng, currentLatLng))
-        }
-    }
-
-    // Funkcja do łączenia pierwszego i ostatniego markera liniami
-    private fun connectFirstAndLastMarker() {
-        val googleMap = mGoogleMap ?: return
-        if (markerPositions.size > 1) {
-            val firstLatLng = markerPositions.first()
-            val lastLatLng = markerPositions.last()
-            googleMap.addPolyline(PolylineOptions().add(lastLatLng, firstLatLng))
-        }
-    }
-    fun onFinishDrawingButtonClick(view: View) {
-        // Połącz ostatni marker z pierwszym
-        connectFirstAndLastMarker()
-        // Zapisz figurę
-        savePolygon()
-    }
-
-    private fun savePolygon() {
-        // Przekonwertuj listę współrzędnych na łańcuch tekstowy
-        val polygonCoordinatesString = convertCoordinatesToString()
-
-        // Uzyskaj dostęp do SharedPreferences
-        val sharedPreferences = this.getSharedPreferences("Polygons", Context.MODE_PRIVATE)
-
-        // Uzyskaj dostęp do edytora SharedPreferences
-        val editor = sharedPreferences.edit()
-
-        // Zapisz współrzędne do SharedPreferences
-        editor.putString("polygon_coordinates", polygonCoordinatesString)
-
-        // Zapisz zmiany
-        editor.apply()
-    }
-
-    private fun convertCoordinatesToString(): String {
-        val stringBuilder = StringBuilder()
-
-        for (latLng in markerPositions) {
-            stringBuilder.append("${latLng.latitude},${latLng.longitude};") // Separatorem może być np. średnik
-        }
-
-        // Usuń ostatni znak separatora, jeśli istnieje
-        if (stringBuilder.isNotEmpty()) {
-            stringBuilder.deleteCharAt(stringBuilder.length - 1)
-        }
-
-        return stringBuilder.toString()
     }
 
 
